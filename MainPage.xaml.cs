59d1beb3c1ba889ca281aa8bf32c990778e62824
TGSAN <h@iloli.tv>
2026-01-09 19:26:03 +0800

    好像能用

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,47 +1,357 @@
 ﻿using MicroWinUICore;
-using Mile.Xaml.Interop;
 using System;
-using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Windows.Media;
 using Windows.Media.Core;
 using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace MicroWinUI
 {
+    [ComImport]
+    [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInitializeWithWindow
+    {
+        void Initialize(IntPtr hwnd);
+    }
+
     public sealed partial class MainPage : Page
     {
-        IslandWindow coreWindowHost;
-        
+        private float _splitterPosition = 0.5f;
+        private bool _isDraggingSplitter = false;
+        private bool _isSeeking = false;
+        private bool _isPlaying = false; // Manual control flag for UI stability
+        private bool _isUpdatingSlider = false;
+        private DispatcherTimer _timer;
+        private TimeSpan _frameStep = TimeSpan.FromMilliseconds(33); // Dynamic frame step
+
+        private MediaPlayer _mp1;
+        private MediaPlayer _mp2;
+        private MediaTimelineController _timelineController;
+        private IslandWindow _host;
+
         public MainPage(IslandWindow coreWindowHost)
         {
-            this.coreWindowHost = coreWindowHost;
             this.InitializeComponent();
+            _host = coreWindowHost;
+            _host.Backdrop = IslandWindow.SystemBackdrop.Mica;
+            InitializePlayers();
+            InitializeTimer();
+        }
+
+        private void InitializePlayers()
+        {
+            // Create a single TimelineController for sync
+            _timelineController = new MediaTimelineController();
+
+            // Initialize Player 1
+            Player1.SetMediaPlayer(new MediaPlayer());
+            _mp1 = Player1.MediaPlayer;
+            _mp1.CommandManager.IsEnabled = false; // Disable individual control
+            _mp1.TimelineController = _timelineController;
+            _mp1.MediaOpened += OnMediaOpened;
+
+            // Initialize Player 2
+            Player2.SetMediaPlayer(new MediaPlayer());
+            _mp2 = Player2.MediaPlayer;
+            _mp2.CommandManager.IsEnabled = false;
+            _mp2.TimelineController = _timelineController;
+        }
+
+        private void InitializeTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(33); // ~30fps UI update
+            _timer.Tick += OnTimerTick;
+            _timer.Start();
+        }
+
+        private void OnTimerTick(object sender, object e)
+        {
+            // Only rely on manual flag. Engine state might be volatile during seeks.
+            if (_isPlaying && !_isSeeking) 
+            {
+                if (_mp1.PlaybackSession.NaturalDuration.TotalSeconds > 0)
+                {
+                    _isUpdatingSlider = true;
+                    TimeSlider.Value = _timelineController.Position.TotalSeconds;
+                    _isUpdatingSlider = false;
+                    UpdateDisplayTime();
+                }
+            }
+        }
+
+        private async void LoadLeftVideo_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await PickVideoFileAsync();
+            if (file != null)
+            {
+                LeftFileText.Text = file.Name;
+                ResetTimeline();
+                // Wrap in MediaPlaybackItem to access track properties
+                var source = MediaSource.CreateFromStorageFile(file);
+                var item = new MediaPlaybackItem(source);
+                _mp1.Source = item;
+            }
+        }
+
+        private async void LoadRightVideo_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await PickVideoFileAsync();
+            if (file != null)
+            {
+                RightFileText.Text = file.Name;
+                ResetTimeline();
+                var source = MediaSource.CreateFromStorageFile(file);
+                var item = new MediaPlaybackItem(source);
+                _mp2.Source = item;
+            }
+        }
+        
+        private void ResetTimeline()
+        {
+            _isPlaying = false;
+            _timelineController.Pause();
+            _timelineController.Position = TimeSpan.Zero;
+            _isUpdatingSlider = true;
+            TimeSlider.Value = 0;
+            _isUpdatingSlider = false;
+            UpdateDisplayTime();
+        }
+
+        private async Task<StorageFile> PickVideoFileAsync()
+        {
+            var picker = new FileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+            picker.FileTypeFilter.Add(".mkv");
+            picker.FileTypeFilter.Add(".avi");
+            picker.FileTypeFilter.Add(".mov");
+            picker.FileTypeFilter.Add(".wmv");
             
-            MediaPlayer.Source = MediaSource.CreateFromUri(new Uri("C:\\Windows\\SystemResources\\Windows.UI.SettingsAppThreshold\\SystemSettings\\Assets\\SDRSample.mkv"));
-            inkToolbar.TargetInkCanvas = inkCanvasDemo;
+            if (_host != null)
+            {
+                var init = (IInitializeWithWindow)(object)picker;
+                init.Initialize(_host.Handle);
+            }
+            
+            return await picker.PickSingleFileAsync();
         }
 
-        private void NoneBackdropRadioButton_Checked(object sender, RoutedEventArgs e)
+        private void OnMediaOpened(MediaPlayer sender, object args)
         {
-            coreWindowHost.Backdrop = IslandWindow.SystemBackdrop.None;
+            Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // Ensure controller is attached when media opens
+                sender.CommandManager.IsEnabled = false;
+                sender.TimelineController = _timelineController;
+
+                if (sender == _mp1)
+                {
+                    // Attempt to get real frame rate
+                    if (sender.Source is MediaPlaybackItem item && item.VideoTracks.Count > 0)
+                    {
+                        var props = item.VideoTracks[0].GetEncodingProperties();
+                        if (props.FrameRate.Numerator > 0 && props.FrameRate.Denominator > 0)
+                        {
+                            double fps = (double)props.FrameRate.Numerator / props.FrameRate.Denominator;
+                            if (fps > 0)
+                            {
+                                _frameStep = TimeSpan.FromSeconds(1.0 / fps);
+                            }
+                        }
+                    }
+
+                    TimeSlider.Maximum = _mp1.PlaybackSession.NaturalDuration.TotalSeconds;
+                    UpdateDisplayTime();
+                }
+            });
+        }
+        
+        private void UpdateDisplayTime()
+        {
+            var current = _timelineController.Position;
+            var total = _mp1.PlaybackSession.NaturalDuration;
+            TimeText.Text = $"{current:hh\\:mm\\:ss\\.fff} / {total:hh\\:mm\\:ss\\.fff}";
         }
 
-        private void AcrylicBackdropRadioButton_Checked(object sender, RoutedEventArgs e)
+        #region Playback Controls
+
+        private void PlayPause_Click(object sender, RoutedEventArgs e)
         {
-            coreWindowHost.Backdrop = IslandWindow.SystemBackdrop.Acrylic;
+            if (_isPlaying)
+            {
+                _isPlaying = false;
+                _timelineController.Pause();
+            }
+            else
+            {
+                _isPlaying = true;
+                _timelineController.Resume();
+            }
         }
 
-        private void MicaBackdropRadioButton_Checked(object sender, RoutedEventArgs e)
+        private void StepForward_Click(object sender, RoutedEventArgs e)
         {
-            coreWindowHost.Backdrop = IslandWindow.SystemBackdrop.Mica;
+            StepFrame(true);
         }
 
-        private void MicaAltBackdropRadioButton_Checked(object sender, RoutedEventArgs e)
+        private void StepBack_Click(object sender, RoutedEventArgs e)
         {
-            coreWindowHost.Backdrop = IslandWindow.SystemBackdrop.Tabbed;
+             StepFrame(false);
         }
+
+        private void StepFrame(bool forward)
+        {
+            _isPlaying = false;
+            _timelineController.Pause();
+
+            TimeSpan step = _frameStep; 
+            if (!forward) step = -step;
+
+            // Use Slider value as current reference to prevent jump-back
+            var currentPos = TimeSpan.FromSeconds(TimeSlider.Value);
+            var newPos = currentPos + step;
+            
+            // Clamp
+            if (newPos < TimeSpan.Zero) newPos = TimeSpan.Zero;
+            var duration = TimeSpan.FromSeconds(TimeSlider.Maximum); 
+            if (newPos > duration) newPos = duration;
+
+            // 1. Update Slider visually (guarded to prevent triggering ValueChanged)
+            _isUpdatingSlider = true;
+            TimeSlider.Value = newPos.TotalSeconds;
+            _isUpdatingSlider = false;
+
+            // 2. Explicitly update Controller to match new Slider value
+            _timelineController.Position = newPos;
+            UpdateDisplayTime();
+        }
+
+        private void TimeSlider_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            _isSeeking = true;
+            _isPlaying = false; // Pause when seeking starts
+            _timelineController.Pause();
+        }
+
+        private void TimeSlider_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            _isSeeking = false;
+            // Explicitly commit the final position to be safe
+            var newPos = TimeSpan.FromSeconds(TimeSlider.Value);
+            _timelineController.Position = newPos;
+        }
+
+        private void TimeSlider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            if (!_isUpdatingSlider)
+            {
+                var newPos = TimeSpan.FromSeconds(e.NewValue);
+                _timelineController.Position = newPos;
+                UpdateDisplayTime();
+            }
+        }
+
+        #endregion
+
+        #region Splitter Logic
+
+        private void UpdateSplitterVisuals()
+        {
+            if (OverlayCanvas.ActualWidth == 0) return;
+
+            var w = OverlayCanvas.ActualWidth;
+            var h = OverlayCanvas.ActualHeight;
+            var x = w * _splitterPosition;
+
+            // Update Line
+            SplitLine.Height = h;
+            Canvas.SetLeft(SplitLine, x - 1); 
+            Canvas.SetTop(SplitLine, 0);
+
+            // Update Thumb
+            double thumbSize = SplitThumb.Width;
+            Canvas.SetLeft(SplitThumb, x - thumbSize / 2);
+            Canvas.SetTop(SplitThumb, h / 2 - thumbSize / 2);
+        }
+
+        private void VideoContainer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateClipRects();
+            UpdateSplitterVisuals();
+        }
+
+        private void UpdateClipRects()
+        {
+            if (VideoContainer.ActualWidth == 0) return;
+
+            double width = VideoContainer.ActualWidth;
+            double height = VideoContainer.ActualHeight;
+            double splitX = width * _splitterPosition;
+
+            var leftClip = new RectangleGeometry();
+            leftClip.Rect = new Windows.Foundation.Rect(0, 0, splitX, height);
+            LeftPlayerContainer.Clip = leftClip;
+
+            var rightClip = new RectangleGeometry();
+            rightClip.Rect = new Windows.Foundation.Rect(splitX, 0, width - splitX, height);
+            RightPlayerContainer.Clip = rightClip;
+        }
+
+        private void OverlayCanvas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            _isDraggingSplitter = true;
+            OverlayCanvas.CapturePointer(e.Pointer);
+        }
+
+        private void OverlayCanvas_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            _isDraggingSplitter = false;
+            OverlayCanvas.ReleasePointerCapture(e.Pointer);
+        }
+
+        private void OverlayCanvas_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (_isDraggingSplitter)
+            {
+                var pt = e.GetCurrentPoint(OverlayCanvas).Position;
+                var w = OverlayCanvas.ActualWidth;
+                
+                _splitterPosition = (float)(pt.X / w);
+                
+                // Clamp
+                if (_splitterPosition < 0) _splitterPosition = 0;
+                if (_splitterPosition > 1) _splitterPosition = 1;
+
+                UpdateClipRects();
+                UpdateSplitterVisuals();
+            }
+        }
+        
+        private void OverlayCanvas_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            // Managed by PointerMoved
+        }
+
+        private void OverlayCanvas_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.SizeWestEast, 1);
+        }
+
+        private void OverlayCanvas_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+             Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.Arrow, 1);
+        }
+
+        #endregion
     }
 }
